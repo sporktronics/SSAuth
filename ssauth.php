723d2e93d8c83a1d59fe6d5a145780026a57745b
Tucker McLean <tuckerm@sporktronics.com>
2012-03-09 22:20:10 -0600

    Added function that sends email to a user

--- ssauth.php
+++ ssauth.php
@@ -1,17 +1,16 @@
 <?php 
 
   /*
-   
+
    Stupid-Simple Authentication
    (c) 2012 Sporktronics, LLC
    http://www.sporktronics.com/
-   
+
    Licensed under the Lesser GPL, version 3.0:
-   http://www.gnu.org/licenses/lgpl-3.0.html
-   
+      http://www.gnu.org/licenses/lgpl-3.0.html
+
   */
 
-// We get our very own Exception?? YAY! 
 class SSAuthException extends Exception { }
 
 class SSAuth {
@@ -20,9 +19,6 @@ class SSAuth {
   private $sessionVar;
   private $users = array();
   
-  // Takes the name of a directory to save user info in. If it's
-  // empty, set it up. If it's not writable, or there's an error
-  // setting it up, throw an exception.
   function __construct($usersDir) {
     session_start();
     
@@ -36,10 +32,7 @@ class SSAuth {
     }
     
   }
-  
-  /* Private functions */
-  
-  // If the users directory is empty, set it up.
+
   private function setupUsersDir() {
     if (!file_exists($this->usersDir.'/users.json')) {      
       return $this->saveUsers();
@@ -49,38 +42,35 @@ class SSAuth {
     }
   }
   
-  // Take the $users array and save it in the users directory.
   private function saveUsers() {
     return (file_put_contents($this->usersDir.'/users.json',
 			      json_encode(array("users" => $this->users))));
   }
-  
-  /* Public functions */
-  
-  // Does the user exist?
-  public function userExists($user) {
-    $userId = $this->getUserId($user);
-    return is_numeric($userId);
-  }
-  
-  // Are we logged in?
+
   public function isLoggedIn() {
     return (!empty($_SESSION[$this->sessionVar]));
   }
   
-  // Read the users.json file and return an array. This is the value
-  // stored in $users.
-  public function getUsers() {
-    return json_decode(file_get_contents($this->usersDir.'/users.json'))->users;
+  public function getLoggedInId() {
+    return getUserId($_SESSION[$this->sessionVar]);
   }
   
-  // Get the path of the users directory.
+  public function getLoggedInUserInfo() {
+    return getUser($_SESSION[$this->sessionVar]);
+  }
+  
+  public function getLoggedInUser() {
+    return $_SESSION[$this->sessionVar];
+  }
+  
   public function getUsersDir() {
     return $this->usersDir;
   }
 
-  // Get the numerical user ID associated with this user false if the
-  // user doesn't exist.
+  public function getUsers() {
+    return json_decode(file_get_contents($this->usersDir.'/users.json'))->users;
+  }
+  
   public function getUserId($user) {
     $userId = array_search($user, $this->users);
     
@@ -91,8 +81,23 @@ class SSAuth {
     }
   }
   
-  // Get the username associated with this numerical user ID, false if
-  // the user ID doesn't exist.
+  public function getDirForUser($user) {
+    $id = $this->getUserId($user);
+    if (is_numeric($id)) {
+      return $this->usersDir."/$id";
+    } else {
+      return false;
+    }
+  }
+  
+  public function getDirForLoggedInUser() {
+    return $this->getDirForUser($this->getLoggedInUser());
+  }
+
+  public function getUser($user) {
+    return $this->getUserById($this->getUserId($user));
+  }
+  
   public function getUserById($userId) {
     if (is_readable($this->usersDir."/$userId/user.json") &&
 	$userjson = file_get_contents($this->usersDir."/$userId/user.json")) {
@@ -104,44 +109,20 @@ class SSAuth {
     }
   } 
   
-  // Get this user's info, false if the user doesn't exist.
-  public function getUser($user) {
-    return $this->getUserById($this->getUserId($user));
-  }
-  
-  // Get the path of the subdirectory of this user, false if they
-  // don't exist.
-  public function getDirForUser($user) {
-    $id = $this->getUserId($user);
-    if (is_numeric($id)) {
-      return $this->usersDir."/$id";
+  public function emailToUser($user, $from, $subject, $message) {
+    $to = $this->getUser($user);
+    if ($to) {
+      return mail($to->email, $subject, $message, "From: $from");
     } else {
       return false;
     }
   }
-  
-  // Get the subdirectory of the logged in user, false if not logged in.
-  public function getDirForLoggedInUser() {
-    return $this->getDirForUser($this->getLoggedInUser());
+
+  public function userExists($user) {
+    $userId = $this->getUserId($user);
+    return is_numeric($userId);
   }
 
-  // Get the user ID of the logged in user, false if not logged in.
-  public function getLoggedInUserId() {
-    return $this->getUserId($_SESSION[$this->sessionVar]);
-  }
-
-  // Get the logged in user's info, false if not logged in.
-  public function getLoggedInUserInfo() {
-    return $this->getUser($_SESSION[$this->sessionVar]);
-  }
-  
-  // Get the logged in username, false if not logged in.
-  public function getLoggedInUser() {
-    return $_SESSION[$this->sessionVar];
-  }
-  
-  // Register a new user. Returns true on success, false if the user
-  // already exists, or throws an exception if there's an error.
   public function register($user, $pass, $email) {
     
     if (!empty($user) && !empty($pass) && !empty($email)) {
@@ -150,10 +131,9 @@ class SSAuth {
 	array_push($this->users, $user);      
 
 	if (!$this->saveUsers()) {
-	  throw new SSAuthException("Couldn't save user list");
+	  throw new SSAuthException('Couldn\'t save user list');
 	}
 	
-	// Create a directory for the user and save their info.
 	try {
 	  $dirForUser = $this->getDirForUser($user);
 	  mkdir($dirForUser);
@@ -165,11 +145,9 @@ class SSAuth {
 										       ))));
 	  return true;
 	} catch (Exception $e) {
-	  // Something bad happened setting up the user.
-	  throw new SSAuthException("Couldn't save user list");
+	  throw new SSAuthException('Couldn\'t save user list');
 	}
       } else {
-	// User already exists.
 	return false;
       }
     } else {
@@ -177,8 +155,6 @@ class SSAuth {
     }
   }
   
-  // Change the password and/or email address of the logged in user.
-  // Returns false if not logged in, throws exception on error.
   public function changeInfo($pass, $email) {
     
     if ($this->isLoggedIn()) {
@@ -195,7 +171,7 @@ class SSAuth {
       
       if (!file_put_contents($this->getDirForUser($userInfo->user)."/user.json",
 			     json_encode(array("user" => $userInfo)))) {
-	throw new SSAuthException("Couldn't change user info");
+	throw new SSAuthException('Couldn\'t change user info');
       } else {
 	return $userInfo;
       }
@@ -206,7 +182,6 @@ class SSAuth {
 
   }
 
-  // Log in, returns true on success, or false on failure.
   public function login($user, $pass) {
     
     if (!empty($user) && !empty($pass)) {
@@ -227,7 +202,6 @@ class SSAuth {
     return false;    
   }
   
-  // Log out, always returns true.
   public function logout() {
     unset($_SESSION[$this->sessionVar]);
     return true;
